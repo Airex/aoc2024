9b9e857e2df9a3f318cc7c2d5a5129736bab4b1a
SKachkovsky <sergii.kachkovskyi@zorallabs.com>
2024-12-10 20:21:42 +0200

    D9 clean up

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -1,12 +1,15 @@
 ﻿module Day9
 
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Jobs
+open BenchmarkDotNet.Running
 open Utils.SampleData
 
 type Disk = | File of int*int | Empty of int
 
 let format arr =
     arr
-    |> Array.map (function | File (fsize, index) -> String.replicate fsize (string index) | Empty esize -> String.replicate esize ".")
+    |> Seq.map (function | File (fsize, index) -> String.replicate fsize (string index) | Empty esize -> String.replicate esize ".")
     |> String.concat ""
     |> dump
 
@@ -29,6 +32,30 @@ let fillGaps input =
                     arr <- arr  |> Array.insertAt (left+1) (Empty (esize-fsize)) // Insert the gap
             | _ -> left <- left + 1 // Move left
     arr
+
+let fillGaps2 (input: Disk array) =
+    let rec fill left right lst =
+        // format lst |> ignore
+        match left > right with
+        | true -> List.rev lst
+        | false ->
+            match input[left] with
+            | File (fsize, index) -> fill (left + 1) right (File (fsize, index) :: lst)
+            | Empty esize ->
+                match input[right] with
+                | Empty _ -> fill left  (right - 1)  lst
+                | File (fsize, index) ->
+                    match fsize <= esize with
+                    | true ->
+                        input.[left] <- Empty (esize - fsize)
+                        fill left (right - 1) (File (fsize, index) :: lst)
+                    | false ->
+                        input[right] <- File (fsize - esize, index)
+                        fill (left + 1)  right (File (esize, index)::lst)
+
+
+    fill 0 ((Array.length input) - 1) []
+
 
 let fillGapsClusters input =
     let mutable arr = input
@@ -67,11 +94,36 @@ let solve gapFiller data =
     |> Seq.sumBy (fun (i, c) -> int64 i * int64 c)
 
 let solution1 = solve fillGaps
+let solution1_2 = solve fillGaps2
 let solution2 = solve fillGapsClusters
 
 let data =
-    loadSampleData "Puzzle2.txt"
+    loadSampleData "Puzzle1.txt"
     |> _.Trim()
 
 
-run [solution1; solution2; ] data
+run [solution1; solution1_2;  solution2; ] data
+
+
+// [<SimpleJob(RuntimeMoniker.Net90)>]
+// [<MemoryDiagnoser>]
+// type Benchmarks() =
+//
+//     member val data = loadSampleData "Puzzle2.txt" |> _.Trim() with get, set
+//
+//     [<Benchmark>]
+//     member this.Solution1() = solution1 this.data
+//
+//     [<Benchmark>]
+//     member this.Solution1_2() = solution1_2 this.data
+//
+//     [<Benchmark>]
+//     member this.Solution2() = solution2 this.data
+//
+// let defaultSwitch () = BenchmarkSwitcher [| typeof<Benchmarks>  |]
+//
+//
+// [<EntryPoint>]
+// let Main args =
+//     defaultSwitch().Run args |> ignore
+//     0
