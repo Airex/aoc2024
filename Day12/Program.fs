47c2181cfaa02551fb257830ea3319427978178e
SKachkovsky <sergii.kachkovskyi@zorallabs.com>
2024-12-12 14:39:29 +0200

    Day12 cleanup

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -6,58 +6,50 @@ open Utils.Pair
 open Utils.SampleData
 
 let directions = [|(1,0); (-1,0); (0,1); (0,-1); |]
-let unpack v = v |> List.unzip |> snd |> List.unzip
 let leftOrRightSide dir = dir = 2 || dir = 3
+let unpackBySide side v = v |> List.unzip |> snd |> List.unzip |> (if leftOrRightSide side then fst else snd)
+let hasGap (a, b) = abs (a - b) > 1
+let gapsCount lst = lst |> Seq.sort |> Seq.pairwise |> Seq.sumBy (hasGap >> zeroOne)
 
 let calculateFencePrice discountFn data =
     let inBounds, get = data |> api
-    let (|PlantOfSameKind|_|) plant x = if (inBounds x |> not) || get x <> plant then Some x else None
+    let (|PlantOfDifferentKind|_|) plant x = if (inBounds x |> not) || get x <> plant then Some x else None
 
     let iterateDirs pos prev found next =
         directions |> Array.fold (fun acc x -> next (pos |> add x) prev acc) found
 
-    let enumerateSides pos =
+    let enumerateSidesNextToOtherPlants pos =
         let plant = get pos
         directions |> Array.indexed |> Array.choose (fun (i, dir) ->
             match (pos |> add dir) with
-            | PlantOfSameKind plant _ -> Some (i, pos)
+            | PlantOfDifferentKind plant _ -> Some (i, pos)
             | _ -> None)
 
-    let rec findPath pos plant (found, visited) =
-        match visited |> Set.contains pos with
-        | true -> (found, visited)
+    let rec findCluster pos plant (found, visited) =
+        match visited |> Set.contains pos, inBounds pos with
+        | true, _ | _, false -> (found, visited)
         | _ ->
-            match inBounds pos with
-            | false -> (found, visited)
-            | true ->
-                match pos |> get = plant with
-                | true -> iterateDirs pos plant (found |> Set.add pos, visited |> Set.add pos) findPath
-                | _ -> (found,  visited |> Set.add pos)
+            match pos |> get = plant with
+            | true -> iterateDirs pos plant (found |> Set.add pos, visited |> Set.add pos) findCluster
+            | _ -> (found,  visited |> Set.add pos)
 
     data |> windowFold (fun clusters (_, x, y, _, _) ->
         if  clusters |> List.exists (Set.contains (x, y)) then
             clusters
         else
-            let cluster, _ = findPath (x, y) (get (x, y)) (Set.empty, Set.empty)
-            ( cluster :: clusters)
+            let cluster = findCluster (x, y) (get (x, y)) (Set.empty, Set.empty) |> fst
+            (cluster :: clusters)
     ) [] (1, 1)
-    |> List.sumBy (S (*) Set.count (Seq.collect enumerateSides >> discountFn))
+    |> List.sumBy (S (*) Set.count (Seq.collect enumerateSidesNextToOtherPlants >> discountFn))
 
 let sidesDiscount sides =
-    let countGaps f lst =
-        let sorted = lst |> unpack |> f |> List.sort
-        let rec countGapsHelper acc = function
-            | [] | [_] -> acc
-            | x::y::xs -> countGapsHelper (acc + (abs(y-x) > 1 |> zeroOne )) (y::xs)
-        (countGapsHelper 0 sorted) + 1
-
-    let sidesOnSameRowOrCol (side, pos) = if leftOrRightSide side then (side, snd pos) else (side, fst pos)
-    let numberOfGapsOnSameRowOrCol ((side, _), v) = countGaps (if leftOrRightSide side then fst else snd) v
+    let sideOnSameRowOrCol (side, pos) = if leftOrRightSide side then (side, snd pos) else (side, fst pos)
+    let numberOfConnectedSidesOnSameRowOrColumn ((side, _), v) = v |> unpackBySide side |> gapsCount |> (+) 1
 
     sides
     |> List.ofSeq
-    |> List.groupBy sidesOnSameRowOrCol
-    |> List.sumBy numberOfGapsOnSameRowOrCol
+    |> List.groupBy sideOnSameRowOrCol
+    |> List.sumBy numberOfConnectedSidesOnSameRowOrColumn
 
 let noDiscount = Seq.length
 
