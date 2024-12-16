95d47ae3469b3c8f24f7ba66e091d7bbc05094c8
SKachkovsky <sergii.kachkovskyi@zorallabs.com>
2024-12-16 14:06:10 +0200

    Day 16

---
+++ Day16/Program.fs
@@ -0,0 +1,55 @@
+﻿module Day16
+
+open Utils
+open Utils.Array2D
+open Utils.Dir
+open Utils.Navigation
+open Utils.Pair
+open Utils.SampleData
+
+let start c = c = 'S'
+let finish c = c = 'E'
+let wall c = c = '#'
+let empty c = c = '.'
+
+let (|Wall|Empty|Finish|)  = function
+    | c when wall c -> Wall
+    | c when empty c -> Empty
+    | c when start c -> Empty
+    | c when finish c -> Finish
+    | _ -> failwith "Invalid character"
+
+let aStarOnMap (map: char array2d) start goal goalReached =
+    let isValid (x, y, _) = map.[x, y] <> '#'
+
+    let neighbors (x, y, dir) =
+        [ Dir.Right; Dir.Left; Dir.Down; Dir.Up ]
+        |> List.map (fun d ->
+            let pos = d |> dirToPair |> add (x, y)
+            fst pos, snd pos, d
+        ) // Uniform cost of 1.0
+        |> List.filter isValid
+        |> List.map (fun (x,y,d) -> (x,y,d), if d = dir then 1.0 else 1001.0) // Uniform cost of 1.0
+
+    let noHeuristics _ _ = 0.0
+    aStar start goal goalReached neighbors noHeuristics
+
+let solve processResult data =
+    let start = data |> findIndex start |> Option.defaultValue (-1, -1)
+    let finish = data |> findIndex finish |> Option.defaultValue (-1, -1)
+
+    let startWithDir = (fst start, snd start, Dir.Right)
+    let goal = (fst finish, snd finish, Dir.Unknown)
+    let goalReached (x, y, _) (gx, gy, _) = x = gx && y = gy
+
+    match aStarOnMap data startWithDir goal goalReached with
+    | Some paths -> paths |> processResult
+    | None -> failwith "No paths found"
+
+let solution1 =  solve (snd >> int)
+let solution2  = solve (fst >> List.collect id >> List.map (fun (x, y, _) -> x, y) >> List.distinct >> List.length)
+
+loadSampleDataLines "Puzzle2.txt"
+|> Array.filter (_.Length >> (<) 0)
+|> array2D
+|> run [ solution1; solution2 ]
---
+++ Utils/Navigation.fs
@@ -0,0 +1,94 @@
+namespace Utils
+
+open System.Collections.Generic
+
+/// Represents a node in the graph.
+type Node<'T> = {
+    Position: 'T
+    GCost: float // Cost from start to this node
+    HCost: float // Heuristic cost to the goal
+    FCost: float // Total cost (G + H)
+    Parent: Node<'T> option // Parent node in the path
+}
+
+module Navigation =
+    /// A* algorithm implementation
+    let aStar
+        (start: 'T)
+        (goal: 'T)
+        (goalReached: 'T -> 'T -> bool) // Function to check if the goal has been reached
+        (neighbors: 'T -> ('T * float) list) // Function to get neighbors and their costs
+        (heuristic: 'T -> 'T -> float) // Heuristic function
+        : ('T list list * float) option = // Returns the paths as a list of nodes, or None if no paths exists
+
+        let openSet = HashSet<Node<'T>>()
+        let closedSet = HashSet<'T>()
+        let nodeMap = Dictionary<'T, Node<'T>>()
+
+        // Create the start node
+        let startNode = {
+            Position = start
+            GCost = 0.0
+            HCost = heuristic start goal
+            FCost = heuristic start goal
+            Parent = None
+        }
+
+        openSet.Add(startNode) |> ignore
+        nodeMap.[start] <- startNode
+
+        let reconstructPath (node: Node<'T>) : 'T list =
+            let rec f node lst =
+                match node.Parent with
+                | Some parent -> f parent (node.Position :: lst)
+                | None -> List.rev (node.Position :: lst)
+            f node []
+
+        let rec loop () =
+            if openSet.Count = 0 then None
+            else
+                // Get the node with the lowest F cost
+                let currentNode = openSet |> Seq.minBy _.FCost
+
+                // If we reached the goal, reconstruct and return the paths with same score
+                if goalReached currentNode.Position goal then
+                    let minCost = currentNode.FCost
+                    let paths = openSet |> Seq.filter (fun x -> x.FCost = minCost) |> Seq.map reconstructPath |> List.ofSeq
+                    Some (paths, currentNode.GCost)
+                else
+                    openSet.Remove(currentNode) |> ignore
+                    closedSet.Add(currentNode.Position) |> ignore
+
+                    // Process each neighbor
+                    for neighborPos, cost in neighbors currentNode.Position do
+                        if not (closedSet.Contains(neighborPos)) then
+                            let tentativeGCost = currentNode.GCost + cost
+
+                            match nodeMap.TryGetValue(neighborPos) with
+                            | true, neighborNode when tentativeGCost < neighborNode.GCost ->
+                                // Update the neighbor's costs and parent
+                                let updatedNeighbor = {
+                                    neighborNode with
+                                        GCost = tentativeGCost
+                                        FCost = tentativeGCost + neighborNode.HCost
+                                        Parent = Some currentNode
+                                }
+                                nodeMap.[neighborPos] <- updatedNeighbor
+                                if not (openSet.Contains(neighborNode)) then
+                                    openSet.Add(updatedNeighbor) |> ignore
+                            | _, _ ->
+                                // Create a new neighbor node
+                                let hCost = heuristic neighborPos goal
+                                let newNode = {
+                                    Position = neighborPos
+                                    GCost = tentativeGCost
+                                    HCost = hCost
+                                    FCost = tentativeGCost + hCost
+                                    Parent = Some currentNode
+                                }
+                                nodeMap.[neighborPos] <- newNode
+                                openSet.Add(newNode) |> ignore
+
+                    loop ()
+
+        loop ()
