b387191aa098ed52e565ca18a8b58a48b2feddc2
SKachkovsky <sergii.kachkovskyi@zorallabs.com>
2024-12-22 15:37:58 +0200

    Day 21 cleanup

--- Day21/Program.fs
+++ Day21/Program.fs
@@ -16,6 +16,7 @@ type Pad(value: char array2d) =
     member this.Button c =
         value |> findIndex ((=) c) |> Option.get
 
+    // less turns are the better, as we do not move to other button = just A on next layer
     member this.OptimalPaths (x0, y0) (x1, y1) =
         let dx = x1 - x0
         let dy = y1 - y0
@@ -38,36 +39,36 @@ let solve layersCount input =
 
     let cache = Array.init pads.Length Dictionary<String, int64>
 
-    let iterate code layer find =
-        let pad = pads[layer]
-
-        let rec fn (code: char list) start len =
-            match code with
-            | [] -> len
-            | c :: rest ->
-                let finish = pad.Button c
-                let s1, s2 = pad.OptimalPaths start finish
-                // get corners to check if we passed trough the gap
-                let c1, c2 = pad.CornersOnOptimalPaths start finish
-
-                let best =
-                    min
-                        (if c1 <> pad.Gap then find s1 (layer + 1) else max64)
-                        (if c2 <> pad.Gap then find s2 (layer + 1) else max64)
-
-                fn rest finish (len + best)
-
-        fn (code |> List.ofSeq) pad.A 0L
-
     let rec find code layer =
-        if (layer = (layersCount + 1)) then
-            code |> String.length |> int64
-        else
+        let iterate code layer =
+            let pad = pads[layer]
+
+            let rec fn (code: char list) start len =
+                match code with
+                | [] -> len
+                | c :: rest ->
+                    let finish = pad.Button c
+                    let s1, s2 = pad.OptimalPaths start finish
+                    // get corners to check if we passed trough the gap
+                    let c1, c2 = pad.CornersOnOptimalPaths start finish
+
+                    let best =
+                        min
+                            (if c1 <> pad.Gap then find s1 (layer + 1) else max64)
+                            (if c2 <> pad.Gap then find s2 (layer + 1) else max64)
+
+                    fn rest finish (len + best)
+
+            fn (code |> List.ofSeq) pad.A 0L
+
+        match layer with
+        | l when l = layersCount + 1 -> code |> String.length |> int64
+        | _ ->
             match cache[layer].TryGetValue(code) with
             | true, v -> v
             | _ ->
-                let len = iterate code layer find
-                cache[layer][code] <- len
+                let len = iterate code layer
+                cache[layer].Add(code, len)
                 len
 
     let codeAsNumber (code: string) = code.TrimEnd('A') |> int64
@@ -77,6 +78,8 @@ let input =
     loadSampleDataLines "Puzzle2.txt"
     |> Array.filter (fun x -> x.Length > 0)
     |> List.ofArray
+
+
 
 let solution1 = solve 2
 let solution2 = solve 25
